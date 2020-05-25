5750c20d6c211699a402e692a391815e9c88c9f4
Kinelan <51113551+Kinelan@users.noreply.github.com>
2020-05-25 12:26:20 +0300

    Class for input direction

---
+++ InputDir.java
@@ -0,0 +1,12 @@
+public class InputDir {
+
+    public int lowerLimit;
+    public int upperLimit;
+    public String folderPath;
+
+    public InputDir(int ll, int ul, String fp){
+        this.lowerLimit = ll;
+        this.upperLimit = ul;
+        this.folderPath = fp;
+    }
+}
